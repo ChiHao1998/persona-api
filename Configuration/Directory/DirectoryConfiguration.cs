cffd64d87425f09877949db2d4ed5b796004d45e
Wong Chi Hao <chihaowong.98@gmail.com>
2026-04-06 16:27:29 +0800

    feat(directory): :sparkles: Add project for host directory configuration
    Introduces a new library to manage and set the host process's current directory during application startup, promoting consistency in app configuration. Enables integration with existing hosting infrastructure via extension methods.

---
+++ Configuration/Directory/DirectoryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Hosting;
+
+namespace Configuration;
+
+public static class DirectoryConfiguration
+{
+    public static IHostBuilder SetHostCurrentDirectory(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            Directory.SetCurrentDirectory(Environment.CurrentDirectory);
+        });
+
+        return hostBuilder;
+    }
+}
