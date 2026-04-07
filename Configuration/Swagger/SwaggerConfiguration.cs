6443dae369ce00fc32e7090022e34038465f7b7c
Wong Chi Hao <chihaowong.98@gmail.com>
2026-04-07 17:39:02 +0800

    feat(configuration): :sparkles: Configure OpenAPI docs and customize Swagger UI behavior
    - configure OpenAPI documentation - enable annotation-based endpoint descriptions - implement reusable Swagger setup and middleware extensions - customize Swagger UI for improved developer experience - limit Swagger UI exposure to development environment

---
+++ Configuration/Swagger/SwaggerConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Configuration;
+
+public static class SwaggerConfiguration
+{
+    public static IServiceCollection SetupSwagger(
+        this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            const string API_DOCUMENTATION_NAME = "Persona Backend API";
+
+            options.EnableAnnotations();
+
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "Version 1",
+                Title = API_DOCUMENTATION_NAME,
+            });
+        });
+        return services;
+    }
+
+    public static WebApplication UseCustomSwagger(this WebApplication app, string title)
+    {
+        app.UseSwagger();
+
+        if (app.Environment.IsDevelopment())
+            app.UseSwaggerUI(configuration =>
+            {
+                configuration.DocumentTitle = $"{title} API Docs";
+                configuration.SwaggerEndpoint("/swagger/v1/swagger.json", $"{title} API V1");
+                configuration.RoutePrefix = "swagger";
+                configuration.DisplayRequestDuration();
+                configuration.EnableTryItOutByDefault();
+                configuration.DocExpansion(DocExpansion.None);
+                configuration.DefaultModelsExpandDepth(-1);
+                configuration.EnablePersistAuthorization();
+                configuration.EnableFilter();
+                configuration.EnableDeepLinking();
+                configuration.ShowExtensions();
+                configuration.EnableSwaggerDocumentUrlsEndpoint();
+            });
+
+        return app;
+    }
+
+}
