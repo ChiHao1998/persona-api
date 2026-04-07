2f2332aa8f5fd320f1e1c29a3e35cdd09529a431
Wong Chi Hao <chihaowong.98@gmail.com>
2026-04-07 14:51:55 +0800

    feat(configuration): :sparkles: Adds generic database broker and unit of work services
    Introduces reusable abstractions for database operations and transaction management using Entity Framework Core. Enhances maintainability by encapsulating common data access and unit of work patterns, supporting dependency injection and transaction handling for various data contexts.

---
+++ Configuration/DatabaseBroker/DatabaseBroker.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Configuration;
+
+public class DatabaseBroker<T>(T dataContext) where T : DbContext
+{
+    protected async ValueTask<TEntity> InsertAsync<TEntity>(TEntity entity)
+        where TEntity : class
+    {
+        await dataContext.Set<TEntity>().AddAsync(entity);
+        return entity;
+    }
+
+    protected TEntity Delete<TEntity>(TEntity entity)
+    where TEntity : class
+    {
+        dataContext.Set<TEntity>().Remove(entity);
+        return entity;
+    }
+
+    protected async ValueTask<IEnumerable<TEntity>> InsertRangeAsync<TEntity>(IEnumerable<TEntity> models)
+        where TEntity : class
+    {
+        await dataContext.Set<TEntity>().AddRangeAsync(models);
+        return models;
+    }
+
+    protected IEnumerable<TEntity> DeleteRange<TEntity>(IEnumerable<TEntity> models)
+        where TEntity : class
+    {
+        dataContext.Set<TEntity>().RemoveRange(models);
+        return models;
+    }
+
+    protected IQueryable<TEntity> Select<TEntity>(bool asNoTracking = true)
+        where TEntity : class
+    {
+        IQueryable<TEntity> query = dataContext.Set<TEntity>();
+
+        return asNoTracking
+            ? query.AsNoTracking()
+            : query;
+    }
+}
