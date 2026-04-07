09921eaa2b9c8310e66d61eda20644631b323eae
ChiHao1998 <97495003+ChiHao1998@users.noreply.github.com>
2026-04-07 16:31:02 +0800

    feat(configuration): :sparkles: Adds response compression configuration support (#12)
    Introduces configuration for response compression with Gzip and enables compression for JSON responses over HTTPS. Improves performance by reducing response sizes and leverages DI for easy integration.

---
+++ Configuration/ResponseCompression/ResponseCompressionConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System.IO.Compression;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Configuration;
+
+public static class ResponseCompressionConfiguration
+{
+    public static IServiceCollection SetupResponseCompression(this IServiceCollection services)
+    {
+        services.AddResponseCompression(options =>
+        {
+            options.EnableForHttps = true;
+            options.Providers.Add<GzipCompressionProvider>();
+            options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                ["application/json"] // compress JSON responses
+            );
+        });
+
+        services.Configure<GzipCompressionProviderOptions>(options =>
+        {
+            options.Level = CompressionLevel.Fastest;
+        });
+
+        return services;
+    }
+}
