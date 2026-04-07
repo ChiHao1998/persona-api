53cb7b173424ef9d5b0eb61a397bb1e8481172c1
ChiHao1998 <97495003+ChiHao1998@users.noreply.github.com>
2026-04-07 16:21:17 +0800

    feat(configuration): :sparkles: Adds Quartz scheduler configuration with PostgreSQL support (#11)
    Introduces a new scheduler setup leveraging Quartz with persistent PostgreSQL storage and clustering enabled. Enhances reliability for scheduled tasks and integrates with the dependency injection system for easier service management and hosting.

---
+++ Configuration/QuartzScheduler/QuartzSchedulerConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace Configuration;
+
+public static class QuartzSchedulerConfiguration
+{
+    public static IServiceCollection SetupQuartzScheduler(this IServiceCollection services, string connectionstrings)
+    {
+        services.AddQuartz(options =>
+        {
+            options.UsePersistentStore(s =>
+            {
+                s.UsePostgres(options =>
+                {
+                    options.TablePrefix = "quartz_";
+                }, connectionstrings);
+                s.UseClustering();
+            });
+
+            options.MisfireThreshold = TimeSpan.FromSeconds(30);
+        });
+
+        services.AddQuartzHostedService(options =>
+        {
+            options.WaitForJobsToComplete = true;
+        });
+
+        return services;
+    }
+}
