61881f41904d17b920bac79bd358700b57a69441
ChiHao1998 <97495003+ChiHao1998@users.noreply.github.com>
2026-04-07 15:05:59 +0800

    feat(configuration): :sparkles: Adds automatic scoped service registration extension (#8)
    Introduces an extension method to scan dependencies and auto-register implementations of a designated scoped service interface, streamlining DI configuration and reducing manual setup. Includes project setup with required dependencies.

---
+++ Configuration/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Common.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AutoAddScopedServiceByIScopedService(this IServiceCollection services)
+    {
+        services.Scan(scan =>
+        {
+            scan
+            .FromApplicationDependencies()
+            .AddClasses(classes => classes.AssignableTo<IScopedService>())
+            .AsImplementedInterfaces()
+            .WithScopedLifetime();
+        });
+
+        return services;
+    }
+}
