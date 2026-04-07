2763b68ec3205006c138e6e0512552add2fdab27
Wong Chi Hao <chihaowong.98@gmail.com>
2026-04-07 11:06:05 +0800

    feat(configuration): :sparkles: Adds API versioning project setup
    Enables consistent API version management and common dependencies across the solution, improving maintainability and scalability.

---
+++ Configuration/ApiVersioning/ApiVersioningConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Configuration;
+
+public static class ApiVersioningConfiguration
+{
+    public static IServiceCollection SetupApiVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(config =>
+        {
+            config.DefaultApiVersion = new ApiVersion(1, 0);
+            config.AssumeDefaultVersionWhenUnspecified = true;
+            config.ReportApiVersions = true;
+        });
+
+        services.AddVersionedApiExplorer(setup =>
+        {
+            setup.GroupNameFormat = "'v'VVV";
+            setup.SubstituteApiVersionInUrl = true;
+        });
+
+        return services;
+    }
+}
